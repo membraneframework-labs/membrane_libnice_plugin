102368e5a130c5d172c7f87f353e53585ccd9fb6
Michał Śledź <sledzson08@gmail.com>
2020-12-21 12:14:02 +0100

    Bump version to 0.2.0. Update deps and README

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.ICE.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @github_url "https://github.com/membraneframework/membrane_ice_plugin"
 
   def project do
@@ -14,7 +14,7 @@ defmodule Membrane.ICE.Plugin.Mixfile do
       deps: deps(),
 
       # hex
-      description: "Membrane Multimedia Framework plugin for ICE",
+      description: "Membrane plugin for ICE protocol",
       package: package(),
 
       # docs
@@ -39,9 +39,9 @@ defmodule Membrane.ICE.Plugin.Mixfile do
       {:membrane_core, "~> 0.6.0"},
       {:unifex, "~> 0.3.2"},
       {:ex_libnice, "~> 0.1.1"},
-      {:ex_doc, "~> 0.22", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
-      {:credo, "~> 1.4", only: :dev, runtime: false},
+      {:credo, "~> 1.5", only: :dev, runtime: false},
       {:membrane_file_plugin, "~> 0.5.0", only: :test},
       {:membrane_hackney_plugin, "~> 0.4.0", only: :test}
     ]
