d07a189a7903dd8c410e4d86b98d437d1505458c
Michał Śledź <michal.sledz@swmansion.com>
2021-08-18 14:37:57 +0200

    Bump ex_libnice to 0.5.1. Bump version to 0.6.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.ICE.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.5.0"
+  @version "0.6.0"
   @github_url "https://github.com/membraneframework/membrane_ice_plugin"
 
   def project do
@@ -37,9 +37,8 @@ defmodule Membrane.ICE.Plugin.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 0.7.0"},
-      {:unifex, "~> 0.4.0"},
       {:bunch, "~> 1.3.0"},
-      {:ex_libnice, "~> 0.4.0"},
+      {:ex_libnice, "~> 0.5.1"},
       {:membrane_funnel_plugin, "~> 0.2.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
