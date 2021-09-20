73bba1de10c256745f54ca67f04f87bc330c4ec4
Michał Śledź <michal.sledz@swmansion.com>
2021-09-20 16:03:11 +0200

    Bump version to 0.7.0. Update deps.

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.ICE.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.6.0"
+  @version "0.8.0"
   @github_url "https://github.com/membraneframework/membrane_ice_plugin"
 
   def project do
@@ -38,7 +38,7 @@ defmodule Membrane.ICE.Plugin.Mixfile do
     [
       {:membrane_core, "~> 0.7.0"},
       {:bunch, "~> 1.3.0"},
-      {:ex_libnice, "~> 0.5.1"},
+      {:ex_libnice, "~> 0.6.0"},
       {:membrane_funnel_plugin, "~> 0.2.0"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
