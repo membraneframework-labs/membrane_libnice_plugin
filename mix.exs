d76a861961ba4cf3d7270afd27b005c6c73abb6c
Michał Śledź <sledzson08@gmail.com>
2021-10-19 15:46:05 +0200

    Bump version to 0.7.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.ICE.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.7.0"
+  @version "0.7.1"
   @github_url "https://github.com/membraneframework/membrane_ice_plugin"
 
   def project do
