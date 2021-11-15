b649a3a5ad01a998819bf965e2f620c17b6ec39d
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2021-11-15 16:04:36 +0100

    Refactor related to comments in PR

--- lib/membrane_ice_plugin/turn_utils.ex
+++ lib/membrane_ice_plugin/turn_utils.ex
@@ -1,6 +1,22 @@
 defmodule Membrane.ICE.TurnUtils do
   @moduledoc false
 
+  @spec generate_credentials(binary(), binary()) :: {binary(), binary()}
+  def generate_credentials(name, secret) do
+    duration =
+      DateTime.utc_now()
+      |> DateTime.to_unix()
+      |> tap(fn unix_timestamp -> unix_timestamp + 24 * 3600 end)
+
+    username = "#{duration}:#{name}"
+
+    password =
+      :crypto.mac(:hmac, :sha, secret, username)
+      |> Base.encode64()
+
+    {username, password}
+  end
+
   @spec generate_secret() :: binary()
   def generate_secret() do
     symbols = '0123456789abcdef'
